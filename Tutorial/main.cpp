d01875532b3ca3060eda25bfca3c35274b8fdaf9
1orwell <elise@eliseratcliffe.co.uk>
2022-03-22 22:00:48 +0000

    learning specific string functions, pretty much the same as python

--- Tutorial/main.cpp
+++ Tutorial/main.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    string name = "Elise Ratcliffe";
+    int age = 26;
+    cout << "Hello " << name << endl;
+    cout << "You are " << age << endl;
+    cout << "There are " << name.length() << " characters in your name" << endl;
+    cout << name.find("cliff", 0) << endl;
+    cout << name.substr(2, 5) << endl;
+
     return 0;
 }
